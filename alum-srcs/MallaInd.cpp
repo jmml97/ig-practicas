5f823d749762ba492dfe50022dfc1dc98f3935ba
José María Martín <jmmartinluque@gmail.com>
2018-09-25 19:00:43 +0200

    Implementados visualizarDE_VBOs, Cubo y Tetraedro

--- alum-srcs/MallaInd.cpp
+++ alum-srcs/MallaInd.cpp
@@ -5,42 +5,33 @@
 // **
 // *********************************************************************
 
+#include "MallaInd.hpp"  // declaración de 'ContextoVis'
 #include <aux.hpp>
 #include <tuplasg.hpp>
-#include "MallaInd.hpp"   // declaración de 'ContextoVis' 
 
 // *****************************************************************************
 // funciones auxiliares
 
-
 // *****************************************************************************
 // métodos de la clase MallaInd.
 
-MallaInd::MallaInd()
-{
-   // 'identificador' puesto a 0 por defecto, 'centro_oc' puesto a (0,0,0)
-   ponerNombre("malla indexada, anónima");
-
+MallaInd::MallaInd() {
+  // 'identificador' puesto a 0 por defecto, 'centro_oc' puesto a (0,0,0)
+  ponerNombre("malla indexada, anónima");
 }
 // -----------------------------------------------------------------------------
 
-MallaInd::MallaInd( const std::string & nombreIni )
-{
-   // 'identificador' puesto a 0 por defecto, 'centro_oc' puesto a (0,0,0)
-   ponerNombre(nombreIni) ;
-
+MallaInd::MallaInd(const std::string& nombreIni) {
+  // 'identificador' puesto a 0 por defecto, 'centro_oc' puesto a (0,0,0)
+  ponerNombre(nombreIni);
 }
 // -----------------------------------------------------------------------------
 // calcula las dos tablas de normales
 
-void MallaInd::calcular_normales()
-{
-   // COMPLETAR: en la práctica 2: calculo de las normales de la malla
-   // .......
-
-
+void MallaInd::calcular_normales() {
+  // COMPLETAR: en la práctica 2: calculo de las normales de la malla
+  // .......
 }
-
 
 // -----------------------------------------------------------------------------
 
@@ -50,18 +41,29 @@ void MallaInd::visualizarDE_MI(ContextoVis& cv) {
 
   glEnableClientState(GL_VERTEX_ARRAY);
   glVertexPointer(3, GL_FLOAT, 0, tabla_cord_vert.data());
-  glDrawElements(GL_TRIANGLES, tabla_indices.size(), GL_UNSIGNED_INT,
-                 tabla_indices.data());
+  glDrawElements(GL_TRIANGLES, tabla_caras.size() * 3, GL_UNSIGNED_INT,
+                 tabla_caras.data());
   glDisableClientState(GL_VERTEX_ARRAY);
 }
 
 // ----------------------------------------------------------------------------
-void MallaInd::visualizarDE_VBOs( ContextoVis & cv )
-{
-   // COMPLETAR: práctica 1: visualizar en modo diferido,
-   //                        usando VBOs (Vertex Buffer Objects)
-   // ..........
+void MallaInd::visualizarDE_VBOs(ContextoVis& cv) {
+  static GLuint id_vbo = 0;
 
+  if (id_vbo == 0) {
+    glGenBuffers(1, &id_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, id_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * tabla_cord_vert.size(),
+                 tabla_cord_vert.data(), GL_STATIC_DRAW);
+  } else {
+    glBindBuffer(GL_ARRAY_BUFFER, id_vbo);
+  }
+
+  glEnableClientState(GL_VERTEX_ARRAY);
+  glVertexPointer(3, GL_FLOAT, 0, 0);
+  glDrawArrays(GL_TRIANGLES, 0, 3 * tabla_cord_vert.size());
+  glDisableClientState(GL_VERTEX_ARRAY);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 // -----------------------------------------------------------------------------
@@ -97,19 +99,19 @@ void MallaInd::visualizarGL(ContextoVis& cv) {
 
 // *****************************************************************************
 
-Cubo::Cubo()
-:  MallaInd( "malla cubo" )
-{
-
+Cubo::Cubo() : MallaInd("malla cubo") {
+  tabla_cord_vert = {{0.0, 0.0, 0.0},  {1.0, 0.0, 0.0}, {1.0, 0.0, 1.0},
+                     {0.0, 0.0, 1.0}, {0.0, 1.0, 0.0}, {1.0, 1.0, 0.0},
+                     {1.0, 1.0, 1.0}, {0.0, 1.0, 1.0}};
+  tabla_caras = {{0, 4, 5}, {0, 1, 5}, {1, 5, 6}, {1, 2, 6},
+                 {2, 6, 7}, {2, 3, 7}, {3, 7, 4}, {3, 0, 4},
+                 {4, 5, 6}, {4, 7, 6}, {0, 1, 2}, {0, 5, 2}};
 }
 // *****************************************************************************
 
-Tetraedro::Tetraedro()
-:  MallaInd( "malla tetraedro")
-{
-
+Tetraedro::Tetraedro() : MallaInd("malla tetraedro") {
+  tabla_cord_vert = {{0.0, 0.0, 0.0},  {1.0, 0.0, 0.0}, {0.0, 0.0, 1.0},
+                     {0.0, 1.0, 1.0}};
+  tabla_caras = {{0, 1, 2}, {0, 1, 3}, {0, 2, 3}, {1, 2, 3}};
 }
 // *****************************************************************************
-
-
-
