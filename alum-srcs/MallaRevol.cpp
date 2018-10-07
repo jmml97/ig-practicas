c1b53621f87b2455aae86cfdf8e5e210f6ab0783
José María Martín <jmmartinluque@gmail.com>
2018-10-07 09:37:55 +0200

    Reorganizadas funciones, implementadas tapas y Cilindro

--- alum-srcs/MallaRevol.cpp
+++ alum-srcs/MallaRevol.cpp
@@ -5,15 +5,17 @@
 // **
 // *********************************************************************
 
+#include "MallaRevol.hpp"
 #include <aux.hpp>
-#include <tuplasg.hpp>
 #include <file_ply_stl.hpp>
 #include <matrices-tr.hpp>
-#include "MallaRevol.hpp"
+#include <tuplasg.hpp>
 
-using namespace std ;
+using namespace std;
 
 // *****************************************************************************
+
+MallaRevol::MallaRevol(const string& nombre) : MallaInd(nombre) {}
 
 MallaRevol::MallaRevol(const std::string& nombre_arch, const unsigned nperfiles,
                        const bool crear_tapas, const bool cerrar_malla) {
@@ -23,37 +25,89 @@ MallaRevol::MallaRevol(const std::string& nombre_arch, const unsigned nperfiles,
   vector<float> vertices;
 
   ply::read_vertices(nombre_arch.c_str(), vertices);
-  nvp = vertices.size()/3;
+  nvp = vertices.size() / 3;
+  nper = nperfiles;
 
-  if (cerrar_malla) {
-    nper = nperfiles;
-  } else {
-    nper = nperfiles - 1;
+  std::vector<Tupla3f> perfil_original;
+
+  for (size_t i = 0; i < vertices.size(); i += 3) {
+    perfil_original.push_back(
+        Tupla3f(vertices[i], vertices[i + 1], vertices[i + 2]));
   }
 
-  std::cout << "nper:" << nper << '\n';
+  generarMallaRevol(perfil_original, crear_tapas, cerrar_malla);
 
-  for (size_t i = 0; i <= nper; i++) {
-    Matriz4f giro = MAT_Rotacion(i * 360 / nper, 0, 1, 0);
-    std::cout << "i: " << i << '\n';
-
-    for (size_t j = 0; j < vertices.size(); j = j + 3) {
-      tabla_cord_vert.push_back(
-          giro * Tupla3f(vertices[j], vertices[j + 1], vertices[j + 2]));
-    }
-  }
-
-  std::cout << "tabla_cord_vert.size(): " << tabla_cord_vert.size() << '\n';
-
-  for (size_t i = 0; i < nper; i++) {
-    for (size_t j = 0; j < nvp - 1; j++) {
-      tabla_caras.push_back(Tupla3u(i*nvp + j, i*nvp + j +1, (i+1)*nvp + j + 1));
-      tabla_caras.push_back(Tupla3u(i*nvp + j, (i+1)*nvp + j, (i+1)*nvp + j + 1));
-    }
-  }
-  
   // calcular la tabla de normales
   calcular_normales();
 }
 
+void MallaRevol::definirMallaRevol(std::vector<Tupla3f> perfil_original,
+                                   const unsigned num_per,
+                                   const unsigned num_vert_per,
+                                   const bool crear_tapas,
+                                   const bool cerrar_malla) {
+  nvp = num_vert_per;
+  nper = num_per;
+
+  generarMallaRevol(perfil_original, crear_tapas, cerrar_malla);
+}
+
+void MallaRevol::generarMallaRevol(std::vector<Tupla3f> perfil_original,
+                                   const bool crear_tapas,
+                                   const bool cerrar_malla) {
+  float angulo_giro = cerrar_malla ? 360.0 / nper : 360.0 / (nper - 1);
+
+  for (size_t i = 0; i <= nper; i++) {
+    Matriz4f giro = MAT_Rotacion(i * angulo_giro, 0, 1, 0);
+
+    for (size_t j = 0; j < nvp; j++) {
+      tabla_cord_vert.push_back(giro * perfil_original[j]);
+    }
+  }
+
+  for (size_t i = 0; i < nper; i++) {
+    for (size_t j = 0; j < nvp - 1; j++) {
+      tabla_caras.push_back(
+          Tupla3u(i * nvp + j, i * nvp + j + 1, (i + 1) * nvp + j + 1));
+      tabla_caras.push_back(
+          Tupla3u(i * nvp + j, (i + 1) * nvp + j, (i + 1) * nvp + j + 1));
+    }
+  }
+
+  if (crear_tapas) {
+    Tupla3f primer_vertice = tabla_cord_vert[0];
+
+    Tupla3f centro_tapa_inferior = Tupla3f(0, primer_vertice[1], 0);
+    tabla_cord_vert.push_back(centro_tapa_inferior);
+
+    for (size_t i = 0; i < nper; i++) {
+      tabla_caras.push_back(
+          Tupla3u(i * nvp, tabla_cord_vert.size() - 1, i * nvp + nvp));
+    }
+
+    Tupla3f ultimo_vertice = tabla_cord_vert[0 + nvp - 1];
+
+    Tupla3f centro_tapa_superior = Tupla3f(0, ultimo_vertice[1], 0);
+    tabla_cord_vert.push_back(centro_tapa_superior);
+
+    for (size_t i = 0; i < nper; i++) {
+      tabla_caras.push_back(Tupla3u(i * nvp + nvp - 1,
+                                    tabla_cord_vert.size() - 1,
+                                    i * nvp + nvp + nvp - 1));
+    }
+  }
+}
+
 // *****************************************************************************
+
+Cilindro::Cilindro(float radio, float altura, int num_per,
+                   const bool crear_tapas, const bool cerrar_malla)
+    : MallaRevol("revolución de un cilindro") {
+  std::vector<Tupla3f> perfil_original;
+
+  for (int i = - (altura / 2); i <= (altura / 2); i++) {
+    perfil_original.push_back(Tupla3f(radio, i, 0));
+  }
+
+  definirMallaRevol(perfil_original, num_per, perfil_original.size(), crear_tapas, cerrar_malla);
+}
--- alum-srcs/MallaRevol.hpp
+++ alum-srcs/MallaRevol.hpp
@@ -8,10 +8,10 @@
 #ifndef IG_MALLAREVOL_HPP
 #define IG_MALLAREVOL_HPP
 
-#include <vector>          // usar std::vector
 #include <string>
+#include <vector>  // usar std::vector
 
-#include "MallaInd.hpp"   // declaración de 'Objeto3D'
+#include "MallaInd.hpp"  // declaración de 'Objeto3D'
 // ---------------------------------------------------------------------
 // clase para objetos gráficos genéricos
 
@@ -21,9 +21,20 @@ class MallaRevol : public MallaInd {
       nvp;        // numero de vertices por perfil
 
  public:
+  MallaRevol(const std::string& nombre);
   // crea una malla de revolucion
   MallaRevol(const std::string& nombre_arch, const unsigned nperfiles,
              const bool crear_tapas, const bool cerrar_malla);
+  void definirMallaRevol(std::vector<Tupla3f> perfil_original,
+                         const unsigned num_per, const unsigned num_vert_per,
+                         const bool crear_tapas, const bool cerrar_malla);
+  void generarMallaRevol(std::vector<Tupla3f> perfil_original,
+                       const bool crear_tapas, const bool cerrar_malla);
 };
 
 #endif
+
+class Cilindro : public MallaRevol {
+ public:
+  Cilindro(float radio, float altura, int num_per, const bool crear_tapas, const bool cerrar_malla);
+};
