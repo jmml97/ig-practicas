97353439737bd8123b7bb7cead2f1e383571049f
José María Martín <jmmartinluque@gmail.com>
2018-10-07 19:44:56 +0200

    Código corregido (generación de caras) y comentado adecuadamente

--- alum-srcs/MallaPLY.cpp
+++ alum-srcs/MallaPLY.cpp
@@ -5,17 +5,20 @@
 // **
 // *********************************************************************
 
-
-#include <cassert>
-#include <aux.hpp>
-#include <tuplasg.hpp>
-#include <file_ply_stl.hpp>
 #include "MallaPLY.hpp"
+#include <aux.hpp>
+#include <cassert>
+#include <file_ply_stl.hpp>
+#include <tuplasg.hpp>
 
-using namespace std ;
+using namespace std;
 
 // *****************************************************************************
 
+/** MallaPLY::MallaPLY() ******************************************************/
+/** Genera un objeto centrado en el origen a partir de la lectura de los     **/
+/** vértices especificados en el archivo                                     **/
+/******************************************************************************/
 MallaPLY::MallaPLY(const std::string& nombre_arch) {
   ponerNombre(string("malla leída del archivo '") + nombre_arch + "'");
 
@@ -24,10 +27,13 @@ MallaPLY::MallaPLY(const std::string& nombre_arch) {
 
   ply::read(nombre_arch.c_str(), vertices, caras);
 
+  /* Por cada 3 números en el archivo, creamos un vértice */
   for (size_t i = 0; i < vertices.size(); i = i + 3) {
-    tabla_cord_vert.push_back(Tupla3f(vertices[i], vertices[i+1], vertices[i+2]));
+    tabla_cord_vert.push_back(
+        Tupla3f(vertices[i], vertices[i + 1], vertices[i + 2]));
   }
 
+  /* Por cada tres índices en el archivo, creamos una cara */
   for (size_t i = 0; i < caras.size(); i = i + 3) {
     tabla_caras.push_back(Tupla3u(caras[i], caras[i + 1], caras[i + 2]));
   }
--- alum-srcs/MallaRevol.cpp
+++ alum-srcs/MallaRevol.cpp
@@ -17,6 +17,10 @@ using namespace std;
 
 MallaRevol::MallaRevol(const string& nombre) : MallaInd(nombre) {}
 
+/** MallaRevol::MallaRevol() ***********************************************/
+/** Lee los vértices del archivo especificado y los almacena en el vector **/
+/** correspondiente, actualizando las variables nvp y nper                **/
+/***************************************************************************/
 MallaRevol::MallaRevol(const std::string& nombre_arch, const unsigned nperfiles,
                        const bool crear_tapas, const bool cerrar_malla) {
   ponerNombre(
@@ -24,23 +28,34 @@ MallaRevol::MallaRevol(const std::string& nombre_arch, const unsigned nperfiles,
 
   vector<float> vertices;
 
+  /* El archivo a leer contiene únicamente la información de los vértices del */
+  /* perfil original                                                          */
   ply::read_vertices(nombre_arch.c_str(), vertices);
   nvp = vertices.size() / 3;
   nper = nperfiles;
 
+  /* Recibimos los vértices como una lista de 3-tuplas de floats y queremos */
+  /* convertir esta información a un vector<Tupla3f>, que es como           */
+  /* almacenamos la información de los vértices                             */
   std::vector<Tupla3f> perfil_original;
 
+  /* Para cada 3-tupla de floats, creamos un vértice */
   for (size_t i = 0; i < vertices.size(); i += 3) {
     perfil_original.push_back(
         Tupla3f(vertices[i], vertices[i + 1], vertices[i + 2]));
   }
 
+  /* La malla de triángulos para las caras las creamos en otra función */
   generarMallaRevol(perfil_original, crear_tapas, cerrar_malla);
 
   // calcular la tabla de normales
   calcular_normales();
 }
 
+/** MallaRevol::definirMallaRevol() *******************************************/
+/** Nos permite actualizar los valores nvp y nper, privados, desde clases    **/
+/** derivadas de esta                                                        **/
+/******************************************************************************/
 void MallaRevol::definirMallaRevol(std::vector<Tupla3f> perfil_original,
                                    const unsigned num_per,
                                    const unsigned num_vert_per,
@@ -52,44 +67,73 @@ void MallaRevol::definirMallaRevol(std::vector<Tupla3f> perfil_original,
   generarMallaRevol(perfil_original, crear_tapas, cerrar_malla);
 }
 
+/** MallaRevol::generarMallaRevol() *******************************************/
+/** Genera las mallas de triángulos que forman las caras a partir del perfil **/
+/** original que se le proporcione                                           **/
+/******************************************************************************/
 void MallaRevol::generarMallaRevol(std::vector<Tupla3f> perfil_original,
                                    const bool crear_tapas,
                                    const bool cerrar_malla) {
+  /* Cerrar la malla es evitar que se superponga el último perfil al primero */
   float angulo_giro = cerrar_malla ? 360.0 / nper : 360.0 / (nper - 1);
 
-  for (size_t i = 0; i <= nper; i++) {
+  /* Insertamos los vértices del perfil original */
+  for (size_t i = 0; i < nvp; i++) {
+    tabla_cord_vert.push_back(perfil_original[i]);
+  }
+
+  /* Para el resto de perfiles, rotamos el perfil y añadimos los vértices  */
+  /* También unimos los vértices de perfiles consecutivos para crear caras */
+  for (size_t i = 1; i < nper; i++) {
     Matriz4f giro = MAT_Rotacion(i * angulo_giro, 0, 1, 0);
 
     for (size_t j = 0; j < nvp; j++) {
       tabla_cord_vert.push_back(giro * perfil_original[j]);
     }
-  }
 
-  for (size_t i = 0; i < nper; i++) {
+    /* Unimos los vértices del perfil actual con los del perfil anterior */
     for (size_t j = 0; j < nvp - 1; j++) {
       tabla_caras.push_back(
-          Tupla3u(i * nvp + j, i * nvp + j + 1, (i + 1) * nvp + j + 1));
+          Tupla3u((i - 1) * nvp + j, (i - 1) * nvp + j + 1, i * nvp + j + 1));
       tabla_caras.push_back(
-          Tupla3u(i * nvp + j, (i + 1) * nvp + j, (i + 1) * nvp + j + 1));
+          Tupla3u((i - 1) * nvp + j, i * nvp + j, i * nvp + j + 1));
+    }
+  }
+
+  /* Si se cierra la malla, unimos los vértices del último perfil con los de */
+  /* el perfil inicial                                                       */
+  if (cerrar_malla) {
+    for (size_t j = 0; j < nvp - 1; j++) {
+      tabla_caras.push_back(
+          Tupla3u((nper - 1) * nvp + j, (nper - 1) * nvp + j + 1, j + 1));
+      tabla_caras.push_back(Tupla3u((nper - 1) * nvp + j, j, j + 1));
     }
   }
 
   if (crear_tapas) {
+    /* Obtenemos el primer vértice del perfil original */
     Tupla3f primer_vertice = tabla_cord_vert[0];
 
+    /* Centramos el vértice en el eje de giro */
     Tupla3f centro_tapa_inferior = Tupla3f(0, primer_vertice[1], 0);
-    tabla_cord_vert.push_back(centro_tapa_inferior);
 
+    /* Añadimos el vértice a la tabla y generamos las caras uniendo los */
+    /* primeros vértices de cada perfil con él                          */
+    tabla_cord_vert.push_back(centro_tapa_inferior);
     for (size_t i = 0; i < nper; i++) {
       tabla_caras.push_back(
           Tupla3u(i * nvp, tabla_cord_vert.size() - 1, i * nvp + nvp));
     }
 
-    Tupla3f ultimo_vertice = tabla_cord_vert[0 + nvp - 1];
+    /* Obtenemos el último vértice del perfil original */
+    Tupla3f ultimo_vertice = tabla_cord_vert[nvp - 1];
 
+    /* Centramos el vértice en el eje de giro */
     Tupla3f centro_tapa_superior = Tupla3f(0, ultimo_vertice[1], 0);
-    tabla_cord_vert.push_back(centro_tapa_superior);
 
+    /* Añadimos el vértice a la tabla y generamos las caras uniendo los */
+    /* primeros vértices de cada perfil con él                          */
+    tabla_cord_vert.push_back(centro_tapa_superior);
     for (size_t i = 0; i < nper; i++) {
       tabla_caras.push_back(Tupla3u(i * nvp + nvp - 1,
                                     tabla_cord_vert.size() - 1,
@@ -100,6 +144,10 @@ void MallaRevol::generarMallaRevol(std::vector<Tupla3f> perfil_original,
 
 // *****************************************************************************
 
+/** Cilindro::Cilindro() ******************************************************/
+/** Genera un cilindro centrado en el origen según el radio y la altura      **/
+/** especificadas.                                                           **/
+/******************************************************************************/
 Cilindro::Cilindro(float radio, float altura, int num_per,
                    const bool crear_tapas, const bool cerrar_malla)
     : MallaRevol("revolución de un cilindro") {
@@ -113,6 +161,10 @@ Cilindro::Cilindro(float radio, float altura, int num_per,
                     crear_tapas, cerrar_malla);
 }
 
+/** Cono::Cono() **************************************************************/
+/** Genera un cono centrado en el origen según el número de perfiles y el    **/
+/** número de vértices por perfil                                            **/
+/******************************************************************************/
 Cono::Cono(int num_per, int num_vert_per, const bool crear_tapas,
            const bool cerrar_malla)
     : MallaRevol("revolución de un cono") {
@@ -129,6 +181,10 @@ Cono::Cono(int num_per, int num_vert_per, const bool crear_tapas,
                     crear_tapas, cerrar_malla);
 }
 
+/** Esfera::Esfera() **********************************************************/
+/** Genera una esfera centrada en el origen según el número de perfiles y el **/
+/** número de vértices por perfil                                            **/
+/******************************************************************************/
 Esfera::Esfera(int num_per, int num_vert_per, const bool crear_tapas,
                const bool cerrar_malla)
     : MallaRevol("revolución de una esfera") {
