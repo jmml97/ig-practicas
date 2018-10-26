9c48f982b9b629f2a15307a9f0acb31058a5f91e
José María Martín <jmmartinluque@gmail.com>
2018-10-26 20:08:28 +0200

    Implementado clase Parametro

--- alum-srcs/Parametro.cpp
+++ alum-srcs/Parametro.cpp
@@ -1,94 +1,98 @@
+#include "Parametro.hpp"
 #include <cassert>
 #include <cmath>
-#include "Parametro.hpp"
 #include "Objeto3D.hpp"
 
 // -----------------------------------------------------------------------------
 
 // crear grado de libertad no acotado
 
-Parametro::Parametro(
-   const std::string &  p_descripcion,
-   Matriz4f *           p_ptr_mat,
-   TFuncionCMF          p_fun_calculo_matriz,
-   bool                 p_acotado,
-   float                p_c,
-   float                p_s,
-   float                p_f
-)
+Parametro::Parametro(const std::string& p_descripcion, Matriz4f* p_ptr_mat,
+                     TFuncionCMF p_fun_calculo_matriz, bool p_acotado,
+                     float p_c, float p_s, float p_f)
+    : descripcion(p_descripcion),
+      ptr_mat(p_ptr_mat),
+      fun_calculo_matriz(p_fun_calculo_matriz),
+      acotado(p_acotado),
+      c(p_c),
+      s(p_s),
+      f(p_f)
 
 {
-   // COMPLETAR: práctica 3: inicializar un parámetro
-   // ....
-
+  // COMPLETAR: práctica 3: inicializar un parámetro
+  // ....
+  valor_norm = 0;
+  velocidad = 1; /* v0 = 1 */
 }
 // -----------------------------------------------------------------------------
 
-void Parametro::siguiente_cuadro()
-{
-   // COMPLETAR: práctica 3: actualizar el valor y la matriz para el siguiente cuadro
-   // ....
-
+void Parametro::siguiente_cuadro() {
+  // COMPLETAR: práctica 3: actualizar el valor y la matriz para el siguiente
+  // cuadro
+  // ....
+  valor_norm += velocidad;
 }
 // -----------------------------------------------------------------------------
 
-void Parametro::reset()
-{
-   // COMPLETAR: práctica 3: hace reset de valor y velocidad
-   // ....
-
+void Parametro::reset() {
+  // COMPLETAR: práctica 3: hace reset de valor y velocidad
+  // ....
+  valor_norm = c;
+  velocidad = v0;
+  /* Recalculamos la matriz ya que ha variado el valor del parámetro */
+  *ptr_mat = fun_calculo_matriz(valor_norm);
 }
 // -----------------------------------------------------------------------------
-void Parametro::incrementar()
-{
-   // COMPLETAR: práctica 3: incrementa el valor
-   // ....
-
-}
-// -----------------------------------------------------------------------------
-
-void Parametro::decrementar()
-{
-   // COMPLETAR: práctica 3: decrementa el valor
-   // ....
-
-}
-// -----------------------------------------------------------------------------
-void Parametro::acelerar()
-{
-   // COMPLETAR: práctica 3: incrementa la velocidad
-   // ....
-
-}
-// -----------------------------------------------------------------------------
-void Parametro::decelerar()
-{
-   // COMPLETAR: práctica 3: decrementa la velocidad
-   // ....
-
+void Parametro::incrementar() {
+  // COMPLETAR: práctica 3: incrementa el valor
+  // ....
+  valor_norm += delta;
+  /* Recalculamos la matriz ya que ha variado el valor del parámetro */
+  *ptr_mat = fun_calculo_matriz(valor_norm);
 }
 // -----------------------------------------------------------------------------
 
-float Parametro::leer_valor_actual()
-{
-   // COMPLETAR: práctica 3: devuelve el valor actual
-   // ....
+void Parametro::decrementar() {
+  // COMPLETAR: práctica 3: decrementa el valor
+  // ....
+  valor_norm += delta;
+  /* Recalculamos la matriz ya que ha variado el valor del parámetro */
+  *ptr_mat = fun_calculo_matriz(valor_norm);
+}
+// -----------------------------------------------------------------------------
+void Parametro::acelerar() {
+  // COMPLETAR: práctica 3: incrementa la velocidad
+  // ....
+  velocidad += a;
+}
+// -----------------------------------------------------------------------------
+void Parametro::decelerar() {
+  // COMPLETAR: práctica 3: decrementa la velocidad
+  // ....
+  velocidad += a;
+}
+// -----------------------------------------------------------------------------
 
+float Parametro::leer_valor_actual() {
+  // COMPLETAR: práctica 3: devuelve el valor actual
+  // ....
+  if (acotado)
+    return c + s * sin(f * 2 * M_PI * valor_norm);
+  else
+    return c + s * valor_norm;
 }
 
 // -----------------------------------------------------------------------------
-float Parametro::leer_velocidad_actual()
-{
-   // COMPLETAR: práctica 3: devuelve la velocidad actual
-   // ....
-
+float Parametro::leer_velocidad_actual() {
+  // COMPLETAR: práctica 3: devuelve la velocidad actual
+  // ....
+  return velocidad;
 }
 // -----------------------------------------------------------------------------
 
-std::string Parametro::leer_descripcion()
-{
-   // COMPLETAR: práctica 3: devuelve la descripción
-   // ....
-
+std::string Parametro::leer_descripcion() {
+  // COMPLETAR: práctica 3: devuelve la descripción
+  // ....
+  return descripcion;
 }
 // -----------------------------------------------------------------------------
--- alum-srcs/Parametro.hpp
+++ alum-srcs/Parametro.hpp
@@ -1,45 +1,56 @@
-#include <string>
 #include <functional>
 #include <matrizg.hpp>
+#include <string>
 
 #ifndef GRADO_LIBERTAD_HPP
 #define GRADO_LIBERTAD_HPP
 
-class Objeto3D ;
-
+class Objeto3D;
 
 // tipo usado para las funciones que calculan una matriz a partir de un flotante
-typedef std::function< Matriz4f( float )> TFuncionCMF  ;
+typedef std::function<Matriz4f(float)> TFuncionCMF;
 
 // -----------------------------------------------------------------------------
 
-class Parametro
-{
-   private:
-   // COMPLETAR: práctica 3: definir variables de instancia privadas de un parámetro
+class Parametro {
+ private:
+  // COMPLETAR: práctica 3: definir variables de instancia privadas de un
+  // parámetro
 
-   public:
-   // crear grado de libertad no acotado
-   Parametro
-   (  const std::string &  p_descripcion,
-      Matriz4f *           p_ptr_mat,
-      TFuncionCMF          p_fun_calculo_matriz,
-      bool                 p_acotado,
-      float                p_c,
-      float                p_s,
-      float                p_f
-   );
+  const std::string descripcion;        /* Descripción del grado de libertad */
+  const bool acotado;                   /* true: el valor oscila entre dos   */ 
+                                        /* valores                           */
+  const TFuncionCMF fun_calculo_matriz; /* Función que produce una nueva     */
+                                        /* matriz a partir de un valor float */
+  
+  const float c, /* Valor inicial (central para grados acotados)       */
+      s,         /* Semiamplitud (si acotado) o factor de escala si no */
+      f;         /* Si acotado, frecuencia                             */
 
-   void  siguiente_cuadro();   // actualizar valor y matriz al siguiente frame
-   void  reset();        // vuelve al estado inicial
-   void  incrementar();  // incrementar el valor
-   void  decrementar() ; // decrementar el valor
-   void  acelerar();     // acelerar (aumentar velocidad normalizada)
-   void  decelerar();    // decelerar (disminuir la velocidad normalizada)
-   float leer_valor_actual(); // devuelve el valor actual (escalado, no normalizado)
-   float leer_velocidad_actual();    // devuelve velocidad o frecuencia actual
-   std::string
-         leer_descripcion();
+  Matriz4f* const ptr_mat; /* Puntero a la matriz dentro del modelo */
+  float valor_norm, /* Valor actual, normalizado y no acotado (crece desde 0) */
+      velocidad;    /* Velocidad actual (se suma a valor_norm) */
+
+  static constexpr float v0 = 0.1;
+  static constexpr float delta = 0.1;
+  static constexpr float a = 0.1;
+
+ public:
+  // crear grado de libertad no acotado
+  Parametro(const std::string& p_descripcion, Matriz4f* p_ptr_mat,
+            TFuncionCMF p_fun_calculo_matriz, bool p_acotado, float p_c,
+            float p_s, float p_f);
+
+  void siguiente_cuadro();  // actualizar valor y matriz al siguiente frame
+  void reset();             // vuelve al estado inicial
+  void incrementar();       // incrementar el valor
+  void decrementar();       // decrementar el valor
+  void acelerar();          // acelerar (aumentar velocidad normalizada)
+  void decelerar();         // decelerar (disminuir la velocidad normalizada)
+  float
+  leer_valor_actual();  // devuelve el valor actual (escalado, no normalizado)
+  float leer_velocidad_actual();  // devuelve velocidad o frecuencia actual
+  std::string leer_descripcion();
 };
 
 #endif
