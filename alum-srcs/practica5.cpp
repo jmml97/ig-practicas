e50020d7b220bd6cc052c9949f29f80eadddc008
José María Martín <jmmartinluque@gmail.com>
2019-01-19 09:03:44 +0100

    practica5: implementación de teclas y movimiento de cámara, inicializar escena

--- alum-srcs/practica5.cpp
+++ alum-srcs/practica5.cpp
@@ -13,44 +13,60 @@
 #include "CamaraInter.hpp"
 #include "grafo-escena.hpp"
 
-using namespace std ;
+using namespace std;
 
-// COMPLETAR: práctica 5: declarar variables de la práctica 5 (static)
-//    (escena, viewport, el vector de camaras, y las variables
-//      que sirven para gestionar el modo arrastrar)
-// ......
+// Declaración de variables de la práctica 5 (static) (escena, viewport, el
+// vector de camaras, y las variables que sirven para gestionar el modo
+// arrastrar).
 
-// número de cámaras
-static constexpr int numero_camaras = 4;
-// vector de cámaras con vista como mínimo de alzado, planta y perfil
-static CamaraInteractiva* camaras[numero_camaras] = {nullptr, nullptr, nullptr,
+// número de cámaras.
+static constexpr int NUMERO_CAMARAS = 4;
+// vector de cámaras con vista como mínimo de alzado, planta y perfil.
+static CamaraInteractiva* camaras[NUMERO_CAMARAS] = {nullptr, nullptr, nullptr,
                                                      nullptr};
 static int camara_activa = 0;   
 
-// viewport actual (se recalcula al inicializar y al fijar las matrices)
+// viewport actual (se recalcula al inicializar y al fijar las matrices).
 static Viewport viewport;
 
-// viewport actual (se recalcula al inicializar y al fijar las matrices)
-Viewport viewport ;
-// true si se está en modo arrastrar, false si no
+// true si se está en modo arrastrar, false si no.
 static bool modo_arrastrar = false ;
 
-// grafo de escena
+// grafo de escena.
 static constexpr int NUMERO_OBJETOS_P5 = 1;
 static unsigned objeto_activo_p5 = 0;
 static NodoGrafoEscena* objetos_p5[NUMERO_OBJETOS_P5] = {nullptr};
 static ColeccionFuentesP4* fuentes_luz_p5 = nullptr;
 
+// Desplazamiento de la cámara.
+static const float DELTA = 5.0;
+
+// Posiciones anteriores del ratón.
+static int x_anterior, y_anterior;
 
 // ---------------------------------------------------------------------
 
-void P5_Inicializar(  int vp_ancho, int vp_alto )
-{
-   cout << "Creando objetos de la práctica 5 .... " << flush ;
-   // COMPLETAR: práctica 5: inicializar las variables de la práctica 5 (incluyendo el viewport)
-   // .......
+void P5_Inicializar(int vp_ancho, int vp_alto) {
+  cout << "Creando objetos de la práctica 5 .... " << flush;
+  // Inicialización de las variables de la práctica 5 (incluyendo el viewport).
+  fuentes_luz_p5 = new ColeccionFuentesP4;
+  objetos_p5[0] = new EscenaP4;
+  viewport = Viewport(0, 0, vp_ancho, vp_alto);
+  float ratio = (float)vp_alto / vp_ancho;
+  // Alzado
+  camaras[0] =
+      new CamaraInteractiva(true, ratio, 0, 0, {0, 0, 0}, true, 80.0, 1.8);
+  // Perfil
+  camaras[1] =
+      new CamaraInteractiva(true, ratio, 90, 0, {0, 0, 0}, true, 80.0, 2.0);
+  // Planta
+  camaras[2] =
+      new CamaraInteractiva(true, ratio, 0, 90, {0, 0, 0}, true, 80.0, 4.0);
+  // Alzado ortográfica
+  camaras[3] =
+      new CamaraInteractiva(true, ratio, 0, 0, {0, 0, 0}, false, 80.0, 3.2);
 
-   cout << "hecho." << endl << flush ;
+  cout << "hecho." << endl << flush;
 }
 // ---------------------------------------------------------------------
 
@@ -78,79 +94,82 @@ void P5_DibujarObjetos(ContextoVis& cv) {
 
 // ---------------------------------------------------------------------
 
-bool P5_FGE_PulsarTeclaCaracter(  unsigned char tecla )
-{
+bool P5_FGE_PulsarTeclaCaracter(unsigned char tecla) {
+  bool result = true;
 
-   bool result = true ;
+  bool examinar;
 
-   switch ( toupper( tecla ) )
-   {
-      case 'C':
-         // COMPLETAR: práctica 5: activar siguiente camara
-         // .....
+  switch (toupper(tecla)) {
+    case 'C':
+      // Activar siguiente cámara.
+      camara_activa = (camara_activa + 1) % NUMERO_CAMARAS;
+      cout << "práctica 5: la nueva cámara activa es " << camara_activa << endl;
+      break;
 
-         break ;
+    case 'V':
+      // Conmutar la cámara actual entre modo examinar y el modo primera
+      // persona.
+      examinar = camaras[camara_activa]->examinar;
+      if (examinar) {
+        camaras[camara_activa]->modoPrimeraPersona();
+      } else {
+        camaras[camara_activa]->modoExaminar();
+        cout << "práctica 5: el modo de la cámara " << camara_activa
+             << " ha cambiado a " << (examinar ? "primera persona" : "examinar")
+             << endl;
+      }
+      break;
 
-      case 'V':
-         // COMPLETAR: práctica 5: conmutar la cámara actual entre modo examinar y el modo primera persona
-         // .....
+    case '-':
+      // Desplazamiento en Z de la cámara actual (positivo) (desplaZ).
+      camaras[camara_activa]->desplaZ(DELTA);
+      cout << "práctica 5: desplazamiento en el eje Z (acercar)" << endl;
+      break;
 
-         break ;
+    case '+':
+      // Desplazamiento en Z de la cámara actual (negativo) (desplaZ).
+      camaras[camara_activa]->desplaZ(-DELTA);
+      cout << "práctica 5: desplazamiento en el eje Z (alejar)" << endl;
+      break;
 
-      case '-':
-         // COMPLETAR: práctica 5: desplazamiento en Z de la cámara actual (positivo) (desplaZ)
-         // .....
-
-         break;
-
-      case '+':
-         // COMPLETAR: práctica 5: desplazamiento en Z de la cámara actual (negativo) (desplaZ)
-         // .....
-
-         break;
-
-      default:
-         result = false ;
-         break ;
-	}
-   return result ;
+    default:
+      result = false;
+      break;
+  }
+  return result;
 }
 // ---------------------------------------------------------------------
 
-bool P5_FGE_PulsarTeclaEspecial(  int tecla  )
-{
+bool P5_FGE_PulsarTeclaEspecial(int tecla) {
+  bool result = true;
 
-   bool result = true ;
+  switch (tecla) {
+    case GLFW_KEY_LEFT:
+      // Desplazamiento/rotación hacia la izquierda (moverHV).
+      camaras[camara_activa]->moverHV(-DELTA, 0);
+      cout << "práctica 5: desplazamiento hacia la izquierda" << endl;
+      break;
+    case GLFW_KEY_RIGHT:
+      // Desplazamiento/rotación hacia la derecha (moverHV).
+      camaras[camara_activa]->moverHV(DELTA, 0);
+      cout << "práctica 5: desplazamiento hacia la derecha" << endl;
+      break;
+    case GLFW_KEY_UP:
+      // Desplazamiento/rotación hacia arriba (moverHV).
+      camaras[camara_activa]->moverHV(0, DELTA);
+      cout << "práctica 5: desplazamiento hacia arriba" << endl;
+      break;
+    case GLFW_KEY_DOWN:
+      // Desplazamiento/rotación hacia abajo (moverHV).
+      camaras[camara_activa]->moverHV(0, -DELTA);
+      cout << "práctica 5: desplazamiento hacia abajo" << endl;
+      break;
+    default:
+      result = false;
+      break;
+  }
 
-
-   switch ( tecla )
-   {
-      case GLFW_KEY_LEFT:
-         // COMPLETAR: práctica 5: desplazamiento/rotacion hacia la izquierda (moverHV)
-         // .....
-
-         break;
-      case GLFW_KEY_RIGHT:
-         // COMPLETAR: práctica 5: desplazamiento/rotación hacia la derecha (moverHV)
-         // .....
-
-         break;
-      case GLFW_KEY_UP:
-         // COMPLETAR: práctica 5: desplazamiento/rotación hacia arriba (moverHV)
-         // .....
-
-         break;
-      case GLFW_KEY_DOWN:
-         // COMPLETAR: práctica 5: desplazamiento/rotación hacia abajo (moverHV)
-         // .....
-
-         break;
-      default:
-         result = false ;
-         break ;
-	}
-
-   return result ;
+  return result;
 }
 // ---------------------------------------------------------------------
 // se llama al hacer click con el botón izquierdo
@@ -177,30 +196,32 @@ void P5_ClickIzquierdo( int x, int y )
 // ---------------------------------------------------------------------
 // se llama al mover el botón en modo arrastrar
 
-void P5_InicioModoArrastrar( int x, int y )
-{
-   // COMPLETAR: práctica 5: activar bool de modo arrastrar, registrar (x,y) de inicio del modo arrastrar
-   // .....
-
+void P5_InicioModoArrastrar(int x, int y) {
+  // Activar bool de modo arrastrar, registrar (x,y) de inicio del modo
+  // arrastrar
+  modo_arrastrar = true;
+  x_anterior = x;
+  y_anterior = y;
 }
 // ---------------------------------------------------------------------
 // se llama al subir el botón derecho del ratón
 
 void P5_FinModoArrastrar()
 {
-   // COMPLETAR: práctica 5: desactivar bool del modo arrastrar
-   // .....
-
+   // Desactivar bool del modo arrastrar
+   modo_arrastrar = false;
 }
 // ---------------------------------------------------------------------
 // se llama durante el modo arrastrar
 
-void P5_RatonArrastradoHasta( int x, int y )
-{
-   // COMPLETAR: práctica 5: calcular desplazamiento desde inicio de modo arrastrar, actualizar la camara (moverHV)
-   // .....
+void P5_RatonArrastradoHasta(int x, int y) {
+  // Calcular desplazamiento desde inicio de modo arrastrar, actualizar la
+  // camara (moverHV) mover cámara
+  camaras[camara_activa]->moverHV(x - x_anterior, y - y_anterior);
 
-
+  // registrar última posición
+  x_anterior = x;
+  y_anterior = y;
 }
 // ---------------------------------------------------------------------
 // pulsar/levantar botón del ratón, específico de la práctica 5
@@ -232,12 +253,10 @@ bool P5_FGE_RatonMovidoPulsado( int x, int y )
 }
 // ---------------------------------------------------------------------
 
-bool P5_FGE_Scroll( int direction )
-{
-   // COMPLETAR: práctica 5: acercar/alejar la camara (desplaZ)
-   // .....
-
-   return true ;
+bool P5_FGE_Scroll(int direction) {
+  // Acercar/alejar la camara (desplaZ)
+  camaras[camara_activa]->desplaZ(direction * DELTA);
+  return true;
 }
 // ---------------------------------------------------------------------
 
